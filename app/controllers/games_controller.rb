76eb3c1a3c48853aa103c7793a0def63dd89485e
Hugo Ferrier <hu.ferrier@gmail.com>
2024-05-03 13:16:12 +0200

    finished ish the exercise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    @letters = []
+    10.times { @letters << ("A".."Z").to_a.sample}
+    @letters
   end
 
+
   def score
+    @letters = params[:letters].split
+    @answer = (params[:answer] || "").upcase
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    raw_json = URI.open(url).read
+    @result = JSON.parse(raw_json)
+    @included = included?(@answer, @letters)
+
   end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
 end
